dad8f59efa4cd21871eeccb3ba88643396589f35
Linus <lsjn88@gmail.com>
2022-05-05 10:30:59 +0200

    Adding string/drawable resource annotation and imageResourceId

--- app/src/main/java/com/c0njur3r/affirmation2/model/Affirmation.kt
+++ app/src/main/java/com/c0njur3r/affirmation2/model/Affirmation.kt
@@ -1,4 +1,9 @@
 package com.c0njur3r.affirmation2.model
 
-data class Affirmation(val stringResourceId: Int) {
-}
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Affirmation(
+    @StringRes val stringResourceId: Int,
+    @DrawableRes val imageResourceId: Int
+    )
